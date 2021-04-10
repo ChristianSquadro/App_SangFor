a20bfaa8566304f39660f0cc65c41ba4e4160ef3
DESKTOP-SKAUFG6\CHRI <christian.squadro@gmail.com>
2021-04-10 18:23:54 +0200

    WebView inserted

--- lib/routes/WebView_Page.dart
+++ lib/routes/WebView_Page.dart
@@ -1,6 +1,6 @@
 import 'package:app_sangfor/cache/UrlConsoleCache.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:provider/provider.dart';
 
 class WebViewConsole extends StatefulWidget {
@@ -11,27 +11,12 @@ class WebViewConsole extends StatefulWidget {
 }
 
 class WebViewConsoleState extends State<WebViewConsole> {
-  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ));
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Consumer<UrlConsoleCache>(builder: (_, value, __) {
-      return InAppWebView(
-        initialUrlRequest: URLRequest(url: Uri.parse("https://inappwebview.dev/")),
-        initialOptions: options,
-        onReceivedServerTrustAuthRequest: (controller, challenge) async {
-          return ServerTrustAuthResponse(action: ServerTrustAuthResponseAction.PROCEED);
-        },
+      return WebviewScaffold(
+        url: value.url,
+        ignoreSSLErrors: true,
       );
     }));
   }
